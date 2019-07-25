23db9f6890e731bf2e55c8eab987e15963a93477
comcometal <example@example.com>
2019-07-26 00:09:00 +0900

    In progress

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,13 @@
-
-
 const main = () => {
     (window as any).a = 3;
-    chrome.alarms.create('MINUTE', {periodInMinutes: 1});
-    chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.alarms.create('MINUTE', { periodInMinutes: 1 });
+    chrome.alarms.onAlarm.addListener(alarm => {
         (window as any).a = (window as any).a + 1;
         console.log((window as any).a);
         if (alarm.name === 'MINUTE') {
             chrome.tabs.create({ url: 'https://mail.google.com/mail/' });
         }
-    })
+    });
 };
 
 (() => {
--- src/components/edit/editModal.ts
+++ src/components/edit/editModal.ts
@@ -1,27 +1,96 @@
-import { getSimpleReducerAndActionCreator, SimpleAction } from './../../utilities/simpleReducer';
 import { combineReducers } from 'redux';
-const UPDATE_URL = '@edit/UPDATE_URL'
-const UPDATE_NAME = '@edit/UPDATE_NAME'
-const UPDATE_PERIOD = '@edit/UPDATE_PERIOD'
+const UPDATE_URL = '@edit/UPDATE_URL';
+const UPDATE_NAME = '@edit/UPDATE_NAME';
+const UPDATE_PERIOD = '@edit/UPDATE_PERIOD';
+const SAVE_RULE = '@edit/SAVE_RULE';
 
-export type EditModalTypes = SimpleAction<string>;
-
-export interface EditModalState {
-    period: string
-    name: string
-    url: string
+interface NameAction {
+    type: typeof UPDATE_NAME;
+    name: string;
 }
 
-const nameRAC = getSimpleReducerAndActionCreator('', UPDATE_NAME);
-const urlRAC  = getSimpleReducerAndActionCreator('', UPDATE_URL);
-const periodRAC  = getSimpleReducerAndActionCreator('', UPDATE_PERIOD);
+interface UrlAction {
+    type: typeof UPDATE_URL;
+    url: string;
+}
 
-export const updateName = nameRAC.actionCreator;
-export const updateURL = urlRAC.actionCreator;
-export const updatePeriod = periodRAC.actionCreator;
+interface PeriodAction {
+    type: typeof UPDATE_PERIOD;
+    period: string;
+}
+
+interface SaveAction {
+    type: typeof SAVE_RULE;
+}
+
+export type EditModalTypes = NameAction | UrlAction | PeriodAction | SaveAction;
+
+export interface EditModalState {
+    period: string;
+    name: string;
+    url: string;
+}
+
+const nameReducer = (state = '', action: EditModalTypes) => {
+    switch (action.type) {
+        case UPDATE_NAME:
+            return action.name;
+        case SAVE_RULE:
+            return '';
+        default:
+            return state;
+    }
+};
+
+const urlReducer = (state = '', action: EditModalTypes) => {
+    switch (action.type) {
+        case UPDATE_URL:
+            return action.url;
+        case SAVE_RULE:
+            return '';
+        default:
+            return state;
+    }
+};
+
+const periodReducer = (state = '', action: EditModalTypes) => {
+    switch (action.type) {
+        case UPDATE_NAME:
+            return action.name;
+        case SAVE_RULE:
+            return '';
+        default:
+            return state;
+    }
+};
+
+export const updateName = (name: string): NameAction => {
+    return {
+        type: UPDATE_NAME,
+        name,
+    };
+};
+export const updateUrl = (url: string): UrlAction => {
+    return {
+        type: UPDATE_URL,
+        url,
+    };
+};
+export const updatePeriod = (period: string): PeriodAction => {
+    return {
+        type: UPDATE_PERIOD,
+        period,
+    };
+};
+export const saveRule = (period: string): PeriodAction => {
+    return {
+        type: UPDATE_PERIOD,
+        period,
+    };
+};
 
 const editModalReducer = combineReducers<EditModalState, EditModalTypes>({
-    name: nameRAC.reducer,
-    url: urlRAC.reducer,
-    period: periodRAC.reducer
+    name: nameReducer,
+    url: urlReducer,
+    period: periodReducer,
 });
